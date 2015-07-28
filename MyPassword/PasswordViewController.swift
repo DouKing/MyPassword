b8de45c664464ea52edf69a0dc83f95696275f0a
Wu Yikai <wuyikai@secoo.com>
2015-07-28 17:22:11 +0800

    ADD: 添加`PasswordViewController`

---
+++ MyPassword/PasswordViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PasswordViewController.swift
+//  MyPassword
+//
+//  Created by WuYikai on 15/7/27.
+//  Copyright (c) 2015年 secoo. All rights reserved.
+//
+
+import UIKit
+
+class PasswordViewController: UIViewController {
+  
+  @IBOutlet weak var introduceLabel: UILabel!
+  @IBOutlet weak var pwdTextField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  @IBAction func _handleNumberButtonAction(sender: UIButton) {
+  }
+  
+}
